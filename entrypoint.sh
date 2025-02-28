bb74e86b367b262e74afe7afdc9eb969aec500c8
Healer <healer@meobius.com>
2025-02-28 04:04:54 +0300

    qwe

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
-if [[ -z "$@" ]]; then
+if [[ -z "$*" ]]; then
     # Если команда не передана, запускаем Tor
     sudo -u debian-tor tor -f /etc/tor/torrc
 else
     # Если команда передана, выполняем её
-    exec $@
+    exec "$@"
 fi
